3aa89f4325b47bcf10a507f4d0f7e0743835b819
mwoodard <mwoodard@CSCLASS-26.cs.hartford.edu>
2017-12-06 16:05:17 -0500

    more stuff

--- SemesterProject/src/semesterproject/GameObject.java
+++ SemesterProject/src/semesterproject/GameObject.java
@@ -5,13 +5,35 @@
  */
 package semesterproject;
 
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.Graphics;
+
 /**
  *
  * @author mwoodard
  */
 public class GameObject {
     
-    public GameObject(){
-        
+    private int xpo,ypo,width,height,xinc,yinc;
+    
+    public GameObject(int xpo,int ypo,int width,int height,int xinc,int yinc){
+        this.xpo=xpo; this.ypo=ypo; this.width=width; this.height=height;
+        this.xinc=xinc; this.yinc=yinc;
     }
+    
+    public int[] get_position(){int[] r={xpo,ypo}; return r;}
+    public int[] get_size(){int[] r={width,height}; return r;}
+    public int[] get_increment(){int[] r={xinc,yinc}; return r;}
+    
+    public void set_position(int x,int y){this.xpo=x; this.ypo=y;}
+    
+    public void paint(Graphics g){ 
+      Font font = new Font("Times",Font.BOLD, 14);
+      g.setFont(font);
+      g.setColor(Color.WHITE);
+      g.fillOval(xpo,ypo,width,height);
+      g.drawString(xpo+" "+ypo,xpo-width/2,ypo);
+    }
+    
 }
