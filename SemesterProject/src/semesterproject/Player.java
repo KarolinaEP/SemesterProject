440531b5d9310e752f842e0d0cbc73669ec69b9c
mwoodard <mwoodard@CSCLASS-26.cs.hartford.edu>
2017-12-06 16:05:31 -0500

    more stuff

--- SemesterProject/src/semesterproject/Player.java
+++ SemesterProject/src/semesterproject/Player.java
@@ -5,6 +5,22 @@ package semesterproject;
  * @author Karolina Pilip, Matt Woodard, Kaleigh McGuirl
  * @version 4 December 2017
  */
-public class Player {
+public class Player extends GameObject{
+    
+    public Player(int xpo,int ypo,int width,int height,int xinc,int yinc){
+        super(xpo,ypo,width,height,xinc,yinc);
+    }
+    
+    public void set_position(int d){
+        if(d==0){
+            
+        }else if(d==1){
+            
+        }else if(d==2){
+            
+        }else{
+            
+        }
+    }
     
 }
