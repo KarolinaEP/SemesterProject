98fb459de567bb45396b00ef2891b65d0a8d2804
Kaleigh McGuirl <Kaleigh McGuirl@137.49.131.226>
2017-12-11 00:53:43 -0500

    hi

--- SemesterProject/src/semesterproject/Maze.java
+++ SemesterProject/src/semesterproject/Maze.java
@@ -2,6 +2,7 @@ package semesterproject;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.Random;
 import java.util.Scanner;
 
 /**
@@ -11,8 +12,9 @@ import java.util.Scanner;
  */
 public class Maze {
     private int[][] map;
-    
+    private Random rand = new Random(1);
     public Maze(){
+        
        int[][] map = {{0, 0, 0, 0, 1, 0, 0, 0, 0, 0},
        {0, 0, 0, 0, 1, 0, 0, 0 ,0, 0},
        {0, 0, 0, 0 ,1 ,0 ,0 ,0, 0 ,0},
@@ -27,6 +29,7 @@ public class Maze {
    }
     
     public Maze (File file, int size) {
+        this.rand = new Random(1);
         int i = 0, j = 0;
         map = new int[size][size];
         try {
@@ -51,4 +54,12 @@ public class Maze {
     
     public int get_map(int x,int y){return map[x][y];}
     public int[] map_size(){int[] q = {map.length,map[0].length};return q;}
+    public int[][] RandomMaze(){
+        for(int i = 0; i < map.length; i++){
+            for(int j = 0; j < map[i].length; j++){
+                map[i][j] = rand.nextInt();
+            }
+        }
+        return map;
+    }
 }
