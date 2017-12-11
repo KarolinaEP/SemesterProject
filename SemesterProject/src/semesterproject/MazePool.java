0cb0648c0976ec308e5667b21a41db4d3ac81cc7
Linka <Linka@10.0.0.163>
2017-12-11 13:11:25 -0500

    sample maze

--- SemesterProject/src/semesterproject/MazePool.java
+++ SemesterProject/src/semesterproject/MazePool.java
@@ -12,28 +12,41 @@ import java.util.Random;
  * @version December 6, 2017
  */
 public class MazePool {
-    private File[] files;
-    private ArrayList<String> results;
+//    private File[] files;
+//    private ArrayList<String> results;
+//    
+//    public void loadFiles() {
+//        results = new ArrayList<>();
+//        String path = Paths.get(".\\Maze").toString();
+//        files = new File(path).listFiles();
+//        //System.out.println(path);
+//
+//        for (File file : files) {
+//            if (file.isFile()) {
+//                results.add(file.getName());
+//            }
+//        }
+//    }
+//    
+//    
+//    public Maze getMazePuzzle() {
+//        int r = (new Random()).nextInt(results.size());  
+//        System.out.println("r="  + r);                   
+//        return new Maze(files[r]);                  
+//    }
     
-    public void loadFiles() {
-        results = new ArrayList<>();
-        String path = Paths.get(".\\Maze").toString();
-        files = new File(path).listFiles();
-        //System.out.println(path);
-
-        for (File file : files) {
-            if (file.isFile()) {
-                results.add(file.getName());
-            }
-        }
+    public MazePool(){
+        int mazewalls[][] = {
+            {0,1,0,0,1,1,1,0,1,0},
+            {0,1,1,0,0,0,0,0,1,0},
+            {0,0,0,0,1,1,1,0,1,1},
+            {1,0,1,0,0,0,1,0,0,1},
+            {1,0,1,1,1,0,1,1,1,1},
+            {1,0,1,0,1,0,0,1,0,0},
+            {1,0,0,0,1,1,1,1,0,1},
+            {0,0,1,0,0,0,0,1,0,1},
+            {1,1,1,0,1,1,0,0,0,1},
+            {1,0,0,0,0,1,1,1,1,1}};
     }
-    
-    
-    public Maze getMazePuzzle() {
-        int r = (new Random()).nextInt(results.size());  
-        System.out.println("r="  + r);                   
-        return new Maze(files[r]);                  
-    }
-    
     
 }
